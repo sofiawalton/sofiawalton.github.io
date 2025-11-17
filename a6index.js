a81baf4eab9ce74f19a3210a3acdf3f6c85c7e6d
sofiawalton <sofiaw6@uw.edu>
2025-11-16 18:55:22 -0800

    testing changes

--- a6index.js
+++ a6index.js
@@ -1,90 +1,99 @@
-const BAUD_RATE = 9600;
-let port, connectBtn;
+const BAUD_RATE = 9600; // This should match the baud rate in your Arduino sketch
+
+let port, connectBtn; // Declare global variables
 
 function setup() {
-  setupSerial();
+  setupSerial(); // Run our serial setup function (below)
+
+  // Create a canvas that is the size of our browser window.
+  // windowWidth and windowHeight are p5 variables
   createCanvas(windowWidth, windowHeight);
+
+  // p5 text settings. BOLD and CENTER are constants provided by p5.
+  // See the "Typography" section in the p5 reference: https://p5js.org/reference/
   textFont("system-ui", 50);
+  textStyle(BOLD);
   textAlign(CENTER, CENTER);
 }
-
 function draw() {
-  const portIsOpen = checkPort();
-  if (!portIsOpen) return;
+  const portIsOpen = checkPort(); // Check whether the port is open (see checkPort function below)
+  if (!portIsOpen) return; // If the port is not open, exit the draw loop
 
-  let str = port.readUntil("\n");
-  if (str.length == 0) return;
+  let str = port.readUntil("\n"); // Read from the port until the newline
+  if (str.length == 0) return; // If we didn't read anything, return.
 
-  let values = str.trim().split(",");
-  if (values.length < 2) return;
+  // trim the whitespace (the newline) and convert the string to a number
+  let values = str.trim().split(","); // Split the string into an array
+  if (values.length < 2) return; // If we don't have two values, return
 
-  let xVal = Number(values[0]);
-  let yVal = Number(values[1]);
+    const xValue = Number(values[0]);
+    const yValue = Number(values[1]);
 
-  let xPos = map(xVal, 0, 1023, 0, windowWidth);
-  let yPos = map(yVal, 0, 1023, 0, windowHeight);
-noStroke();
-  fill("red");
-  rect(0,0,windowHeight,windowWidth/3);
+    // Map the joystick values to the canvas size
+    const xPos = map(xValue, 0, 1023, 0, windowWidth);
+    const yPos = map(yValue, 0, 1023, 0, windowHeight);
 
-  fill("green");
-  rect(0,windowWidth/3,windowHeight,windowWidth/3);
+    // Clear the background
+    background("lightgrey");
+    FileList("white");
 
-  fill("yellow");
-  rect(0,(2*windowWidth/3),windowHeight,(windowWidth/3));
+    let shape = "";
+    if (xValue < 341) {
+        ellipse(xPos, yPos, 100, 100); // Draw circle
+        shape = "Circle";
+    } else if (xValue < 682) {
+        rect(xPos - 50, yPos - 50, 100, 100); // Draw square
+        shape = "Square";
+    } else {
+        triangle(xPos, yPos - 58, xPos - 50, yPos + 29, xPos + 50, yPos + 29); // Draw triangle
+        shape = "Triangle";
+    }
 
-  let shape = "";
+    port.write(shape + "\n"); // Send the shape name back to Arduino
     
-  if (xVal < 400) {
-    ellipse(xPos, yPos, 100, 100);
-    shape = "circle";
-    fill("red");
-  } else if (xVal > 700) {
-    rect(xPos - 50, yPos - 50, 100, 100);
-    shape = "square";
-    fill("green");
- } 
-//else {
-//     triangle(xPos, yPos - 50, xPos - 50, yPos + 50, xPos + 50, yPos + 50);
-//     shape = "triangle";
-//     fill("yellow");
-//   }
-
-  // Send shape command to Arduino
-  port.write(shape + "\n");
-
-  // Display text
-  fill("black");
-  text(`Shape: ${shape}`, windowWidth / 2, 50);
+    // Display the shape name
+    fill("black");
+    text(shape, windowWidth / 2, 50);
 }
+// Three helper functions for managing the serial connection.
 
-// Serial setup
 function setupSerial() {
   port = createSerial();
+
+  // Check to see if there are any ports we have used previously
   let usedPorts = usedSerialPorts();
   if (usedPorts.length > 0) {
+    // If there are ports we've used, open the first one
     port.open(usedPorts[0], BAUD_RATE);
   }
+
+  // create a connect button
   connectBtn = createButton("Connect to Arduino");
-  connectBtn.position(5, 5);
-  connectBtn.mouseClicked(onConnectButtonClicked);
+  connectBtn.position(5, 5); // Position the button in the top left of the screen.
+  connectBtn.mouseClicked(onConnectButtonClicked); // When the button is clicked, run the onConnectButtonClicked function
 }
 
 function checkPort() {
   if (!port.opened()) {
+    // If the port is not open, change button text
     connectBtn.html("Connect to Arduino");
+    // Set background to gray
     background("gray");
     return false;
   } else {
+    // Otherwise we are connected
     connectBtn.html("Disconnect");
     return true;
   }
 }
 
 function onConnectButtonClicked() {
+  // When the connect button is clicked
   if (!port.opened()) {
+    // If the port is not opened, we open it
     port.open(BAUD_RATE);
   } else {
+    // Otherwise, we close it!
     port.close();
   }
 }
